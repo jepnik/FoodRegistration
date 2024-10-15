fee6aa318413315147512b61a118355669dd9cc6
jombaa <jombaa00@gmail.com>
2024-10-15 13:19:45 +0200

    Kildekode

--- obj/Debug/net8.0/FoodRegistration.AssemblyInfo.cs
+++ obj/Debug/net8.0/FoodRegistration.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodRegistration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9057e2e95540d354a64c60884c1e2ff146073aea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c63a545ff79dcc70b1aaea2109ffdea1657447b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodRegistration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodRegistration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
