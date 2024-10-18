a6730a324da9e75b8be887ebcc05709673cbee94
tine12341 <tine_baasland@outlook.com>
2024-10-18 14:07:28 +0200

    Updatet the database with the new nutrients information

---
+++ Migrations/20241018120342_NutrientsExpanded.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodRegistration.Migrations
+{
+    /// <inheritdoc />
+    public partial class NutrientsExpanded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Carbohydrates",
+                table: "Items",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Energi",
+                table: "Items",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Fat",
+                table: "Items",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Fiber",
+                table: "Items",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Protein",
+                table: "Items",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Salt",
+                table: "Items",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Saturatedfat",
+                table: "Items",
+                type: "REAL",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Sugar",
+                table: "Items",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Unsaturatedfat",
+                table: "Items",
+                type: "REAL",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Carbohydrates",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Energi",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Fat",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Fiber",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Protein",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Saturatedfat",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Sugar",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Unsaturatedfat",
+                table: "Items");
+        }
+    }
+}
