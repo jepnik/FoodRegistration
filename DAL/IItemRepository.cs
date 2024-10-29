b272b4382b03dbfcf00458f198671659661225f1
Hallvard96 <hallvard.berger@gmail.com>
2024-10-29 12:23:36 +0100

    bug fix

--- DAL/IItemRepository.cs
+++ DAL/IItemRepository.cs
@@ -7,6 +7,6 @@ public interface IItemRepository
     Task<IEnumerable<Item>> GetAll();
     Task<Item?> GetItemById(int id);
     Task Create(Item item);
-    Task<bool> Update(Item item);
+    Task Update(Item item);
     Task<bool> Delete(int id);
 }
