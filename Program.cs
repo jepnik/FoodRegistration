1b94f020784b5a25d85f5e4e4a5dc80864fafa4c
Hallvard96 <hallvard.berger@gmail.com>
2024-10-31 10:54:40 +0100

    React

--- Program.cs
+++ Program.cs
@@ -1,31 +1,3 @@
-/* var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run(); */
-
 using Microsoft.EntityFrameworkCore;
 using FoodRegistration.DAL;
 using Serilog;
@@ -42,7 +14,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Use Serilog for logging
 builder.Host.UseSerilog();
 
-builder.Services.AddControllersWithViews();
+// Add services to the container.
+builder.Services.AddControllersWithViews(); // For MVC
+builder.Services.AddControllers(); // For API controllers
 
 // Configure the DbContext with SQLite
 builder.Services.AddDbContext<ItemDbContext>(options =>
@@ -50,17 +24,30 @@ builder.Services.AddDbContext<ItemDbContext>(options =>
     options.UseSqlite(builder.Configuration["ConnectionStrings:ItemDbContextConnection"]);
 });
 
+// Register repository
 builder.Services.AddScoped<IItemRepository, ItemRepository>();
 
 var app = builder.Build();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000") // Change to the port where React is running
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 // Log application start
 Log.Information("Application Starting...");
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    DBInit.Seed(app);
+    DBInit.Seed(app); // Seed the database in development mode
 }
 else
 {
@@ -68,9 +55,19 @@ else
     app.UseHsts();
 }
 
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.MapDefaultControllerRoute();
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Configure route mappings for both MVC and API
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"); // MVC route
+
+app.MapControllers(); // API route mapping
 
 app.Run();
 
