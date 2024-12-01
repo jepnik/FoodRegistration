6b4463911b90b4df1f275df8b511cdf1dc76791a
jepnik <jepnikast@gmail.com>
2024-12-01 18:35:20 +0100

    Added some comments

--- FoodRegistration/Middleware/AuthenticationMiddleware.cs
+++ FoodRegistration/Middleware/AuthenticationMiddleware.cs
@@ -9,7 +9,7 @@ public class AuthenticationMiddleware
 
     public async Task InvokeAsync(HttpContext context)
     {
-        // Check if session exists
+        // Check if session exists, if no session and user is not on RegisterUser or Login page, redirect user to Login page
         if (!context.Session.GetInt32("UserID").HasValue
         && !context.Request.Path.StartsWithSegments("/Account/Login")
         && !context.Request.Path.StartsWithSegments("/Account/RegisterUser"))
